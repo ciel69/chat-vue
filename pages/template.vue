c2f2c04978d115b2a64c88de0aefd15cab3d6dc1
Aleksei Bukhaltsov <Bukhaltsov@lanit.ru>
2020-07-21 16:14:18 +0300

    added style message

--- pages/template.vue
+++ pages/template.vue
@@ -149,8 +149,8 @@
       <v-col md="12" cols="12">
         <v-label>Virtual scroll</v-label>
       </v-col>
-      <v-col md="3" cols="12">
-        <ListMessage />
+      <v-col md="5" cols="12">
+        <ListMessage :items="items" />
       </v-col>
     </v-row>
 
@@ -177,6 +177,7 @@
 
 <script lang="ts">
 import {Component, Vue} from 'nuxt-property-decorator';
+import {v4 as uuidv4} from 'uuid';
 
 import TextareaEmoji from '~/components/TextareaEmoji/TextareaEmoji.vue';
 import Animation from '~/components/Animation.vue';
@@ -190,6 +191,68 @@ import ListMessage from '~/components/ListMessage.vue';
   }
 })
 export default class Template extends Vue {
+
+  items: any[] = [
+    {
+      uid: uuidv4(),
+      text: 'Какой-то текст',
+      user: {
+        firstName: 'User 1'
+      },
+      date: '11.12.19 11:23'
+    },
+    {
+      uid: uuidv4(),
+      text: 'Бертолетова соль монотонно возбуждает газообразный комплексный фторид церия. Континуальность художественного процесса, в том числе, аккумулирует полисахарид. Воображение стационарно имитирует атом, именно об этом комплексе движущих сил писал З.Фрейд в теории сублимации. Атом, по определению, неизменяем.',
+      user: {
+        firstName: 'User 1'
+      },
+      date: '11.12.19 11:23'
+    },
+    {
+      uid: uuidv4(),
+      text: 'Смысл жизни принимает во внимание онтологический гравитационный парадокс. Конфликт может быть получен из опыта.',
+      user: {
+        firstName: 'User 2'
+      },
+      date: '11.12.19 11:23'
+    },
+    {
+      uid: uuidv4(),
+      text: 'Знак, как принято считать, создает дуализм, учитывая опасность, которую представляли собой писания Дюринга для не окрепшего еще немецкого рабочего движения.',
+      user: {
+        firstName: 'User 2'
+      },
+      date: '11.12.19 11:23'
+    },
+    {
+      uid: uuidv4(),
+      text: 'Экситон притягивает вращательный лазер.',
+      user: {
+        firstName: 'User 3'
+      },
+      date: '11.12.19 11:23'
+    },
+    {
+      uid: uuidv4(),
+      text: 'Сознание многопланово отчуждает институциональный психоанализ.',
+      user: {
+        firstName: 'User 4'
+      },
+      date: '11.12.19 11:23'
+    }
+  ];
+
+  created(): void {
+    const length = 1000;
+
+    for (let i = 0; i < length; i++) {
+      this.items.push({
+        uid: `unique_${i}`,
+        text: Math.random() + ' text'
+      });
+    }
+  }
 
   handleSend(message: string): void {
     // eslint-disable-next-line no-console
